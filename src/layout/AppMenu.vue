bd925b59f109af52be3b31451c8f2dcb2536c599
Bobson Lin <bobson.lin@adlinktech.com>
2023-02-03 14:56:13 +0800

    chore: initial project

---
+++ src/layout/AppMenu.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { ref } from 'vue';
+
+import AppMenuItem from './AppMenuItem.vue';
+
+const model = ref([
+    {
+        label: 'Home',
+        items: [{ label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }]
+    },
+    {
+        label: 'Hierarchy',
+        items: [
+            {
+                label: 'Submenu 1',
+                icon: 'pi pi-fw pi-bookmark',
+                items: [
+                    {
+                        label: 'Submenu 1.1',
+                        icon: 'pi pi-fw pi-bookmark',
+                        items: [
+                            { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
+                            { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
+                            { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
+                        ]
+                    },
+                    {
+                        label: 'Submenu 1.2',
+                        icon: 'pi pi-fw pi-bookmark',
+                        items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
+                    }
+                ]
+            },
+            {
+                label: 'Submenu 2',
+                icon: 'pi pi-fw pi-bookmark',
+                items: [
+                    {
+                        label: 'Submenu 2.1',
+                        icon: 'pi pi-fw pi-bookmark',
+                    },
+                    {
+                        label: 'Submenu 2.2',
+                        icon: 'pi pi-fw pi-bookmark',
+                        items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
+                    }
+                ]
+            }
+        ]
+    },
+]);
+</script>
+
+<template>
+    <ul class="layout-menu">
+        <template v-for="(item, i) in model" :key="item">
+            <app-menu-item v-if="!item.separator" :item="item" :index="i"></app-menu-item>
+            <li v-if="item.separator" class="menu-separator"></li>
+        </template>
+        <!-- <li>
+            <a href="https://www.primefaces.org/primeblocks-vue/#/" target="_blank">
+                <img src="/layout/images/banner-primeblocks.png" alt="Prime Blocks" class="w-full mt-3" />
+            </a>
+        </li> -->
+    </ul>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import AppLayout from '@/layout/AppLayout.vue';
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            component: AppLayout,
+            children: [
+                {
+                    path: '/',
+                    name: 'dashboard',
+                    component: () => import('@/views/Dashboard.vue')
+                }
+            ]
+        }
+    ]
+});
+
+export default router;
