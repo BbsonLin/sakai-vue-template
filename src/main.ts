9facc0cabaed22efaf2def9d18d675513931a796
Bobson Lin <bobson801104@gmail.com>
2024-10-02 16:42:41 +0000

    refactor: Delete unused font files and add new variables for light and dark themes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue';
 import 'virtual:uno.css'
 import App from './App.vue';
 import router from './router';
+
+import Aura from '@primevue/themes/aura';
 import PrimeVue from 'primevue/config'
 import ToastService from 'primevue/toastservice'
 
@@ -10,7 +12,14 @@ import '@/assets/styles.scss';
 const app = createApp(App);
 
 app.use(router);
-app.use(PrimeVue, { ripple: true });
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.app-dark'
+        }
+    }
+});
 app.use(ToastService)
 
 app.mount('#app');
